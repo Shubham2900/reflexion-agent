c98899fce814e6f65dab6350759b28fe7d82a4fb
Shubham Bishnoi <shubhambishnoi8797@gmail.com>
2026-08-02 12:24:41 +0530

    Schemas and tool_executor created

---
+++ tool_executor.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_tavily import TavilySearch
+from langchain_core.tools import StructuredTool
+from langgraph_prebuilt import ToolNode
+from schemas import AnswerQuestion, ReviseAnswer
+
+tavily_tool = TavilySearch(max_results=5)
+
+def run_queries(search_queries: list[str], **kwargs):
+    """Run search queries and return the results."""
+    return tavily_tool.batch([{"query": query} for query in search_queries])
+
+execute_tools = ToolNode(
+    [
+        StructuredTool.from_function(run_queries, name = AnswerQuestion.name),
+        StructuredTool.from_function(run_queries, name = ReviseAnswer.name),
+    ]
+)
